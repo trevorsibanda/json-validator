da98350da9b1392ce2b63a9acd5541f731dfa969
Trevor Sibanda <trevorsibb@gmail.com>
2017-05-05 19:43:02 +0200

    Schema validator model

---
+++ app/models/SchemaValidator.scala
@@ -0,0 +1,45 @@
+package models
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.fge.jackson.JsonLoader
+import com.github.fge.jsonschema.core.exceptions.ProcessingException
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.main.{JsonSchema, JsonSchemaFactory}
+
+import scala.util.{Either, Left, Right}
+
+case class Document(val node: JsonNode)
+object Document{
+	def apply(s: String): Document = {
+		new Document(JsonLoader.fromString(s))
+	}
+}
+
+final case class Schema(val node: JsonNode){
+         val schema: JsonSchema = Schema.factory.getJsonSchema(node)
+}
+object Schema{
+	val factory: JsonSchemaFactory = JsonSchemaFactory.byDefault()
+
+	def apply(schema: String): Schema = {
+		new Schema(JsonLoader.fromString(schema))
+	}
+
+	def validate(schema: Schema): ProcessingReport = try factory.getSyntaxValidator().validateSchema(schema.node) catch{
+		case e: Exception => throw e //wrap this
+	}
+
+	def validate(schema: String): ProcessingReport = validate(apply(schema))
+}
+
+case class SchemaValidator(val schema: Schema){
+	def validate(doc: Document): Either[ProcessingReport, Document] = schema.schema.validate(doc.node) match{
+		case report: ProcessingReport if report.isSuccess => Right(doc)
+		case report: ProcessingReport => Left(report)
+	}
+}
+object SchemaValidator{
+	def apply(s: String) = {
+		new SchemaValidator(Schema(s))
+	}
+}
